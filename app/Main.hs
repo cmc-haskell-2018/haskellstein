3c1c0f627988360aa4a9c53a1501b76ff0b95195
Alex Khakhmovich <xax007@mail.ru>
2018-02-28 22:13:17 +0300

    added specCellCond

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,7 @@ import Prelude
 import System.Environment
 import Data
 import Initialize
+import CheckMap
 
 main :: IO ()
 main = do
@@ -16,7 +17,14 @@ main = do
         let (player, _, enemies, edit) = scene
         putStrLn $ "Player x=" ++ show (pPosX player) ++ " y=" ++ show (pPosY player)
         printEnemies enemies
+        let {xcoord = 3; ycoord = 5}
         putStrLn $ "edited map " ++ show edit
+        putStr $ "Cell [" ++ show xcoord ++
+                  ", " ++ show ycoord ++ "] is "
+        case (specCellCond edit (xcoord,ycoord)) of 
+          Blocked -> putStrLn "Blocked cell"
+          Free -> putStrLn "Free cell"
+          Destructible -> putStrLn "Destructible cell"
 
 printEnemies :: [Enemy] -> IO()
 printEnemies [] = putStrLn "end"
