e150b58bdcd110d0427b750b70d0cfe092e47be4
Alex Khakhmovich <xax007@mail.ru>
2018-02-28 22:16:28 +0300

    new

---
+++ src/CheckMap.hs
@@ -0,0 +1,20 @@
+module CheckMap where
+
+import Data
+import Prelude 
+
+--specify step-on-cell perfomance
+specCellCond :: Tilemap -> CellCoord -> CellCond
+specCellCond tm (n,m) = spec (head (dropElements m 
+                                    (head (dropElements n tm)) ))
+  where
+    dropElements :: Int -> [a] -> [a]
+    dropElements _ [] = []
+    dropElements count (x:xs)
+        | count == 0 = (x:xs)
+        | otherwise = dropElements (count - 1) xs
+    spec :: String -> CellCond
+    spec c
+        | head c == 'w' || head c == 'a' || head c == 'o' = Blocked
+        | head c == 'v' || head c == 'i' || head c == 'b' = Free 
+        | otherwise = Destructible
