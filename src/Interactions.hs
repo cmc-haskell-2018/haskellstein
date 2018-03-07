76f8a0e002a44346411826e71a45cc494a40cf40
max101101 <suhov85@mail.ru>
2018-03-07 19:52:11 +0300

    add few enemy function

--- src/Interactions.hs
+++ src/Interactions.hs
@@ -4,6 +4,8 @@ import Prelude
 import Data
 import CheckMap
 
+------------------------FIREBALL_FUNCTIONS----------------------------------
+
 --move fireballs
 stepFireballs :: [Fireball] -> Tilemap -> ([Fireball], Tilemap)
 stepFireballs [] tmap      = ([], tmap)
@@ -23,7 +25,7 @@ stepFireball f tmap = case cond of
                                                   a
                                                   (fDamage f)
                                                   (fRadius f)
-                                                  (fSpeed f)
+                                                  s
                                                   (fModel f)
                                               )
                                         , tmap)
@@ -33,6 +35,7 @@ stepFireball f tmap = case cond of
     x        = fPosX f
     y        = fPosY f
     a        = fRadian f
+    s        = fSpeed f
     delta    = 1 --need to be timer dif
     newx     = x + (delta * s * cos a)
     newy     = y + (delta * s * sin a)
@@ -90,3 +93,80 @@ damageFireball f (e:es)
     rx            = abs (fx - ex)
     ry            = abs (fy - ey)
     (newf, enret) = damageFireball f es
+
+-------------------------------ENEMY_FUNCTIONS------------------------------
+
+--enemies func
+--stepEnemies :: Player -> [Enemy] -> (Player, [Enemy])
+--stepEnemies p []     = (p, [])
+--stepEnemies p (e:es) =
+
+
+
+--moves Enemy to Player
+moveEnemy :: Player -> Enemy -> Tilemap -> Enemy
+moveEnemy p e tmap = case cond of
+                       Free         -> Enemy
+                                           newx
+                                           newy
+                                           (eHp e)
+                                           (eDamage e)
+                                           (eRange e)
+                                           (eSpeed e)
+                                           (eASpeed e)
+                                           (eModel e)
+                                           (eTex e)
+                                           (eVision e)
+                                           (eAgro e)
+                       Destructible -> e
+                       Blocked      -> e
+  where
+    px       = pPosX p
+    py       = pPosY p
+    ex       = ePosX e
+    ey       = ePosY e
+    es       = eSpeed e
+    rx       = (px - ex)
+    ry       = (py - ey)
+    delta    = 1 --need to be timer dif
+    cosalpha = rx/(sqrt ((rx * rx) + (ry * ry)))
+    sinalpha = (sqrt (1 - (cosalpha * cosalpha))) * signum ry
+    newx     = ex + (delta * es * cosalpha)
+    newy     = ey + (delta * es * sinalpha)
+    newcoord = (floor newy, floor newx)
+    cond     = specCellCond tmap newcoord
+
+--is player in vision
+isPInVision :: Player -> Enemy -> Bool
+isPInVision p e = result
+  where
+    px     = pPosX p
+    py     = pPosY p
+    ex     = ePosX e
+    ey     = ePosY e
+    ev     = eVision e
+    rx     = abs (px - ex)
+    ry     = abs (py - ey)
+    result = (rx < ev) && (ry < ev)
+
+--is player in range
+isPInRange :: Player -> Enemy -> Bool
+isPInRange p e = result
+  where
+    px     = pPosX p
+    py     = pPosY p
+    ex     = ePosX e
+    ey     = ePosY e
+    er     = eRange e
+    rx     = abs (px - ex)
+    ry     = abs (py - ey)
+    result = (rx < er) && (ry < er)
+
+stepEnemy :: Player -> Enemy -> (Player, Enemy)
+stepEnemy p e =
+  where
+    px = pPosX p
+    py = pPosY p
+    ph = pHp p
+    ex = ePosX e
+    ey = ePosY e
