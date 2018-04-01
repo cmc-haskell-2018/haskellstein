a75b5259d463b2aa537a3c1a71cd39cceb1dc276
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2018-04-01 04:21:41 +0300

    Add simple texture support

--- src/Haskellstein/Engine.hs
+++ src/Haskellstein/Engine.hs
@@ -4,7 +4,14 @@ module Haskellstein.Engine where
 import SFML.Graphics hiding (rotate)
 import SFML.Window hiding (x, y)
 
+import System.IO.Unsafe
+
 import Haskellstein.Engine.Raycasting
+
+wallTexture :: Texture
+wallTexture = unsafePerformIO $ do
+  Right t <- textureFromFile "textures/wall.png" Nothing
+  return t
 
 run :: IO ()
 run = do
@@ -15,7 +22,7 @@ run = do
             (Just defaultContextSettings)
   let theta = -0.3
       dir = rotate theta (-1, 0)
-      plane = rotate (theta + pi/2) (-1, 0)
+      plane = rotate (theta + pi/2) (-0.66, 0)
       
   loop wnd (Camera (16, 1.5) dir plane)
   destroy wnd
@@ -42,32 +49,38 @@ renderLines camera =
   foldMap (\(i, hs) -> concatMap (renderWall i) (take 1 hs))
     (zip [0..] (raycastWithMap 100 640 sampleMap camera))
   where
-    renderWall i (Hit info _) = 
-      [ Vertex (Vec2f x (240 - y)) wallColor (Vec2f 0 0)
-      , Vertex (Vec2f x (240 + y)) wallColor (Vec2f 0 0)
+    renderWall i (Hit info c) = 
+      [ Vertex (Vec2f x (240 - y)) wallColor (Vec2f texX' 0)
+      , Vertex (Vec2f x (240 + y)) wallColor (Vec2f texX' 63)
       ]
       where
         x = fromIntegral i
         y = 240 / hitDistance info
 
+        texX = hitPosition info * 63
+
+        texX'
+          | c == '#' = texX
+          | otherwise = texX + 64
+
         wallColor =
           case hitSide info of
-            SideX -> Color 100 0 200 255
-            SideY -> Color 50 0 150 255
+            SideX -> Color 255 255 255 255
+            SideY -> Color 128 128 128 255
                       
 
     sampleMap (i, j) = (xs !! j) !! i
       where
         xs = map (map f)
-          [ "##################"
+          [ "#@#@#@#@#@#@#@#@#@"
           , "#                #"
-          , "#######          #"
+          , "#@#@#@#          #"
           , "#   #     #      #"
           , "#   #     #      #"
-          , "#   ####### ######"
+          , "#   #@#@#@# #@#@#@"
           , "#      #         #"
           , "#           #    #"
-          , "##################"
+          , "#@#@#@#@#@#@#@#@#@"
           ]
     
         f ' ' = Nothing
@@ -76,15 +89,15 @@ renderLines camera =
 loop :: RenderWindow -> Camera -> IO ()
 loop wnd camera = do
     clearRenderWindow wnd (Color 32 32 32 255)
-    drawPrimitives wnd (renderLines camera) Lines Nothing
+    drawPrimitives wnd (renderLines camera) Lines (Just renderStates { texture = wallTexture })
     display wnd
     evt <- waitEvent wnd
     case evt of
         Just SFEvtClosed -> return ()
         Just (SFEvtKeyPressed KeyA _ _ _ _) ->
-          loop wnd (rotateCamera 0.1 camera)
-        Just (SFEvtKeyPressed KeyD _ _ _ _) ->
           loop wnd (rotateCamera (-0.1) camera)
+        Just (SFEvtKeyPressed KeyD _ _ _ _) ->
+          loop wnd (rotateCamera 0.1 camera)
         Just (SFEvtKeyPressed KeyW _ _ _ _) ->
           loop wnd (moveCamera 0.1 camera)
         Just (SFEvtKeyPressed KeyS _ _ _ _) ->
