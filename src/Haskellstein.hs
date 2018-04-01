ad50dbeca212498c9689dec57dcb83e18564e558
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2018-04-01 12:58:06 +0300

    Factor out Player

--- src/Haskellstein.hs
+++ src/Haskellstein.hs
@@ -1,54 +1,66 @@
 module Haskellstein where
 
-import SFML.Graphics (textureFromFile)
+import SFML.Graphics (Texture, textureFromFile, setSmooth)
 import SFML.Window.Event
 import SFML.Window.Keyboard
 
 import Haskellstein.Engine
 import Haskellstein.Engine.Raycasting
 
+import Haskellstein.Player
+
+-- | Handle window events.
+handleEvent :: SFEvent -> Player -> Player
+-- set rotation and movement with WASD keys
+handleEvent (SFEvtKeyPressed KeyW _ _ _ _) = setPlayerSpeed 2
+handleEvent (SFEvtKeyPressed KeyA _ _ _ _) = setPlayerRotation (-2)
+handleEvent (SFEvtKeyPressed KeyS _ _ _ _) = setPlayerSpeed (-2)
+handleEvent (SFEvtKeyPressed KeyD _ _ _ _) = setPlayerRotation 2
+-- reset rotation and movement when keys are released
+handleEvent (SFEvtKeyReleased KeyW _ _ _ _) = setPlayerSpeed 0
+handleEvent (SFEvtKeyReleased KeyA _ _ _ _) = setPlayerRotation 0
+handleEvent (SFEvtKeyReleased KeyS _ _ _ _) = setPlayerSpeed 0
+handleEvent (SFEvtKeyReleased KeyD _ _ _ _) = setPlayerRotation 0
+-- ignore all other events
+handleEvent _ = id
+
+-- | A sample map with 'Char' objects in map cells.
+sampleMap :: MapCoords -> Maybe Char
+sampleMap (i, j) = (xs !! j) !! i
+  where
+    xs = map (map f)
+      [ "#@#@#@#@#@#@#@#@#@"
+      , "#                #"
+      , "#@#@#@#          #"
+      , "#   #     #      #"
+      , "#   #     #      #"
+      , "#   #@#@#@# #@#@#@"
+      , "#      #         #"
+      , "#           #    #"
+      , "#@#@#@#@#@#@#@#@#@"
+      ]
+
+    f ' ' = Nothing
+    f c   = Just c
+
+-- | Make a textured map from an ASCII map.
+makeWorldMap :: Texture -> (MapCoords -> Maybe Char) -> (MapCoords -> Maybe Picture)
+makeWorldMap wallTexture m = fmap f . m
+  where
+    f '#' = PictureTexture wallTexture 0  (64, 64)
+    f _   = PictureTexture wallTexture 64 (64, 64)
+
 run :: IO ()
 run = do
   Right wallTexture <- textureFromFile "textures/wall.png" Nothing
-  play engineSettings initWorld (worldMap wallTexture) worldCamera handleEvent updateWorld
+  setSmooth wallTexture True
+  play engineSettings initPlayer (worldMap wallTexture) playerCamera handleEvent updatePlayer
   where
     engineSettings = defaultEngineSettings
       { engineWindowTitle = "Haskellstein"
       , engineWindowSize  = (640, 480)
+      , engineFramerateLimit = Nothing
       }
 
-    initWorld = Camera (15, 1.5) dir plane
-      where
-        theta = -0.3
-        dir = rotate theta (-1, 0)
-        plane = rotate (theta + pi/2) (-0.66, 0)
-
-    worldMap wallTexture _ = sampleMap wallTexture
-    worldCamera = id
-
-    handleEvent (SFEvtKeyPressed KeyW _ _ _ _) = moveCamera 0.1
-    handleEvent (SFEvtKeyPressed KeyA _ _ _ _) = rotateCamera (-0.1)
-    handleEvent (SFEvtKeyPressed KeyS _ _ _ _) = moveCamera (-0.1)
-    handleEvent (SFEvtKeyPressed KeyD _ _ _ _) = rotateCamera 0.1
-    handleEvent _ = id
-
-    updateWorld dt = moveCamera dt . rotateCamera (- dt)
-
-    sampleMap wallTexture (i, j) = (xs !! j) !! i
-      where
-        xs = map (map f)
-          [ "#@#@#@#@#@#@#@#@#@"
-          , "#                #"
-          , "#@#@#@#          #"
-          , "#   #     #      #"
-          , "#   #     #      #"
-          , "#   #@#@#@# #@#@#@"
-          , "#      #         #"
-          , "#           #    #"
-          , "#@#@#@#@#@#@#@#@#@"
-          ]
-    
-        f '#' = Just (PictureTexture wallTexture 0  (64, 64))
-        f '@' = Just (PictureTexture wallTexture 64 (64, 64))
-        f _ = Nothing
+    worldMap wallTexture _ = makeWorldMap wallTexture sampleMap
 
---
+++ src/Haskellstein/Player.hs
@@ -0,0 +1,40 @@
+module Haskellstein.Player where
+
+import Data.Function ((&))
+import Haskellstein.Engine.Raycasting
+
+-- | Player state.
+data Player = Player
+  { playerCamera    :: Camera   -- ^ Player camera location and direction.
+  , playerRotation  :: Float    -- ^ Player rotation speed.
+  , playerSpeed     :: Float    -- ^ Player movement speed.
+  }
+
+-- | Initialise 'Player'.
+initPlayer :: Player
+initPlayer = Player
+  { playerCamera = Camera (15, 1.5) dir plane
+  , playerRotation = 0
+  , playerSpeed    = 0
+  }
+  where
+    theta = -0.3
+    dir = rotate theta (-1, 0)
+    plane = rotate (theta + pi/2) (-0.66, 0)
+
+-- | Set 'Player' rotation speed.
+setPlayerRotation :: Float -> Player -> Player
+setPlayerRotation theta player = player { playerRotation = theta }
+
+-- | Set 'Player' movement speed.
+setPlayerSpeed :: Float -> Player -> Player
+setPlayerSpeed speed player = player { playerSpeed = speed }
+
+-- | Update 'Player': move and rotate.
+updatePlayer :: Float -> Player -> Player
+updatePlayer dt player = player { playerCamera = newCamera }
+  where
+    newCamera = playerCamera player
+      & moveCamera (dt * playerSpeed player)
+      & rotateCamera (dt * playerRotation player)
+
