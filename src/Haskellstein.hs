fcc54052ed01649a07decbb4aeb9faba4fd10dfd
Nickolay Kudasov <nickolay.kudasov@gmail.com>
2018-04-01 05:09:18 +0300

    Introduce play function to encourage pure game design

--- src/Haskellstein.hs
+++ src/Haskellstein.hs
@@ -1,5 +1,52 @@
-module Haskellstein (
-  run,
-) where
+module Haskellstein where
+
+import SFML.Graphics (Color(..), textureFromFile)
+import SFML.Window.Event
+import SFML.Window.Keyboard
 
 import Haskellstein.Engine
+import Haskellstein.Engine.Raycasting
+
+run :: IO ()
+run = do
+  Right wallTexture <- textureFromFile "textures/wall.png" Nothing
+  play title winSize bgColor raycasterDepth initWorld (worldMap wallTexture) worldCamera handleEvent
+  where
+    title = "Haskellstein"
+    winSize = (640, 480)
+    bgColor = Color 32 32 32 255
+    raycasterDepth = 100
+
+    initWorld = Camera (16, 1.5) dir plane
+      where
+        theta = -0.3
+        dir = rotate theta (-1, 0)
+        plane = rotate (theta + pi/2) (-0.66, 0)
+
+    worldMap wallTexture _ = sampleMap wallTexture
+    worldCamera = id
+
+    handleEvent (SFEvtKeyPressed KeyW _ _ _ _) = moveCamera 0.1
+    handleEvent (SFEvtKeyPressed KeyA _ _ _ _) = rotateCamera (-0.1)
+    handleEvent (SFEvtKeyPressed KeyS _ _ _ _) = moveCamera (-0.1)
+    handleEvent (SFEvtKeyPressed KeyD _ _ _ _) = rotateCamera 0.1
+    handleEvent _ = id
+
+    sampleMap wallTexture (i, j) = (xs !! j) !! i
+      where
+        xs = map (map f)
+          [ "#@#@#@#@#@#@#@#@#@"
+          , "#                #"
+          , "#@#@#@#          #"
+          , "#   #     #      #"
+          , "#   #     #      #"
+          , "#   #@#@#@# #@#@#@"
+          , "#      #         #"
+          , "#           #    #"
+          , "#@#@#@#@#@#@#@#@#@"
+          ]
+    
+        f '#' = Just (PictureTexture wallTexture 0  (64, 64))
+        f '@' = Just (PictureTexture wallTexture 64 (64, 64))
+        f _ = Nothing
+
--- src/Haskellstein/Engine.hs
+++ src/Haskellstein/Engine.hs
@@ -1,39 +1,69 @@
-{-# OPTIONS_GHC -fno-warn-type-defaults #-}
 module Haskellstein.Engine where
 
 import SFML.Graphics hiding (rotate)
-import SFML.Window hiding (x, y)
-
-import System.IO.Unsafe
+import SFML.Window hiding (x, y, windowWidth, windowHeight)
 
 import Haskellstein.Engine.Raycasting
 
-wallTexture :: Texture
-wallTexture = unsafePerformIO $ do
-  Right t <- textureFromFile "textures/wall.png" Nothing
-  return t
+data Picture
+  = PictureTexture Texture Float (Float, Float)
 
-run :: IO ()
-run = do
+-- | Play a game with raycasting rendering.
+play
+  :: String                                 -- ^ Window title.
+  -> (Int, Int)                             -- ^ Window size.
+  -> Color                                  -- ^ Background color.
+  -> Int                                    -- ^ Raycaster depth.
+  -> world                                  -- ^ Initial world state.
+  -> (world -> MapCoords -> Maybe Picture)  -- ^ World map.
+  -> (world -> Camera)                      -- ^ Player camera.
+  -> (SFEvent -> world -> world)            -- ^ Event handler.
+  -> IO ()
+play title windowSize bgColor raycasterDepth initWorld worldMap worldCamera handleEvent = do
   wnd <- createRenderWindow
-            defaultVideoMode
-            "Haskellstein"
+            (VideoMode windowWidth windowHeight 32)
+            title
             [SFDefaultStyle]
             (Just defaultContextSettings)
-  let theta = -0.3
-      dir = rotate theta (-1, 0)
-      plane = rotate (theta + pi/2) (-0.66, 0)
-      
-  loop wnd (Camera (16, 1.5) dir plane)
+  loop wnd initWorld
   destroy wnd
+  where
+    (windowWidth, windowHeight) = windowSize
 
-defaultVideoMode :: VideoMode
-defaultVideoMode = VideoMode
-  { windowWidth  = 640
-  , windowHeight = 480
-  , windowBPP    = 32
-  }
+    loop wnd world = do
+      clearRenderWindow wnd bgColor
+      renderLines wnd world
+      display wnd
 
+      evt <- waitEvent wnd
+      case evt of
+          Just SFEvtClosed -> return ()
+          Just event -> loop wnd (handleEvent event world)
+          _ -> loop wnd world
+
+    renderLines wnd world =
+      mapM_ (\(i, hs) -> mapM_ (renderWall i) (take 1 hs))
+        (zip [0..] (raycastWithMap raycasterDepth windowWidth sampleMap camera))
+      where
+        camera = worldCamera world
+        sampleMap = worldMap world
+
+        renderWall x (Hit info (PictureTexture wallTexture offset (texW, texH))) = do
+          drawPrimitives wnd 
+            [ Vertex (Vec2f x (h / 2 - y)) wallColor (Vec2f texX 0)
+            , Vertex (Vec2f x (h / 2 + y)) wallColor (Vec2f texX (texH - 1))
+            ] Lines (Just renderStates { texture = wallTexture })
+          where
+            h = fromIntegral windowHeight
+            y = h / 2 / hitDistance info
+
+            texX = offset + hitPosition info * (texW - 1)
+
+            wallColor =
+              case hitSide info of
+                SideX -> Color 255 255 255 255
+                SideY -> Color 128 128 128 255
+                          
 defaultContextSettings :: ContextSettings
 defaultContextSettings = ContextSettings
   { depthBits         = 24
@@ -44,62 +74,3 @@ defaultContextSettings = ContextSettings
   , attributeFlags    = [ContextDefault]
   }
 
-renderLines :: Camera -> [Vertex]
-renderLines camera =
-  foldMap (\(i, hs) -> concatMap (renderWall i) (take 1 hs))
-    (zip [0..] (raycastWithMap 100 640 sampleMap camera))
-  where
-    renderWall i (Hit info c) = 
-      [ Vertex (Vec2f x (240 - y)) wallColor (Vec2f texX' 0)
-      , Vertex (Vec2f x (240 + y)) wallColor (Vec2f texX' 63)
-      ]
-      where
-        x = fromIntegral i
-        y = 240 / hitDistance info
-
-        texX = hitPosition info * 63
-
-        texX'
-          | c == '#' = texX
-          | otherwise = texX + 64
-
-        wallColor =
-          case hitSide info of
-            SideX -> Color 255 255 255 255
-            SideY -> Color 128 128 128 255
-                      
-
-    sampleMap (i, j) = (xs !! j) !! i
-      where
-        xs = map (map f)
-          [ "#@#@#@#@#@#@#@#@#@"
-          , "#                #"
-          , "#@#@#@#          #"
-          , "#   #     #      #"
-          , "#   #     #      #"
-          , "#   #@#@#@# #@#@#@"
-          , "#      #         #"
-          , "#           #    #"
-          , "#@#@#@#@#@#@#@#@#@"
-          ]
-    
-        f ' ' = Nothing
-        f c = Just c
-
-loop :: RenderWindow -> Camera -> IO ()
-loop wnd camera = do
-    clearRenderWindow wnd (Color 32 32 32 255)
-    drawPrimitives wnd (renderLines camera) Lines (Just renderStates { texture = wallTexture })
-    display wnd
-    evt <- waitEvent wnd
-    case evt of
-        Just SFEvtClosed -> return ()
-        Just (SFEvtKeyPressed KeyA _ _ _ _) ->
-          loop wnd (rotateCamera (-0.1) camera)
-        Just (SFEvtKeyPressed KeyD _ _ _ _) ->
-          loop wnd (rotateCamera 0.1 camera)
-        Just (SFEvtKeyPressed KeyW _ _ _ _) ->
-          loop wnd (moveCamera 0.1 camera)
-        Just (SFEvtKeyPressed KeyS _ _ _ _) ->
-          loop wnd (moveCamera (-0.1) camera)
-        _ -> loop wnd camera
